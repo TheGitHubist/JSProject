7bc264d2b179551e5df9a43cb110452289e164fd
Nathan <nathan.champilou@ynov.com>
2024-05-23 16:21:13 +0200

    function pour afficherune  carte et la déplacer avec la souris sa souris

--- client/src/scenes/Game.js
+++ client/src/scenes/Game.js
@@ -63,12 +63,12 @@ export class Game extends Scene
         this.load.image('Titouan', '../assets/monstres/Titouan.png');
         this.load.image('Tu_veux_mon_sandwitch', '../assets/monstres/Tu_veux_mon_sandwitch.png');
 
-        // Back of Card
-        this.load.image('back', '../assets/png-clipart-yu-gi-oh-skin-texture-mapping-pattern-field-soccer-field-rectangle-symmetry-thumbnail.png');
+        // Back of field
+        this.load.image('field', '../assets/png-clipart-yu-gi-oh-skin-texture-mapping-pattern-field-soccer-field-rectangle-symmetry-thumbnail.png');
     }
     create () {
 
-        this.add.image(645, 390, 'back').setScale(3.7, 2.8);
+        this.add.image(645, 390, 'field').setScale(3.7, 2.8);
         this.card = this.add.image( 300, 300, 'Tu_veux_mon_sandwitch' ).setScale(0.1, 0.1).setInteractive();
         this.input.on('drag', function (pointer, gameObject, dragX, dragY)  {
             gameObject.x = dragX;
